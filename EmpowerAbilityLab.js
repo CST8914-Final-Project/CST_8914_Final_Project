3da5fc56922d01eec8a1bff7510e203cf54f5795
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-03-23 15:33:42 -0400

    put functionality imputs in the form and added a function that change the switch toggle.

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -5,7 +5,16 @@ function knowledgeRunner(){
 }
 
 
-
+function toggleChange() {
+    var imageTag = document.getElementById('switchToggle').src;
+    imageTag = imageTag.replace(/^.*[\\\/]/, 'images/');
+    window.alert(imageTag);
+    if (imageTag === "images/toggle_switch_off.jpg") {
+        document.getElementById('switchToggle').src = 'images/toggle_switch_on.jpg';
+    } else {
+        document.getElementById('switchToggle').src = 'images/toggle_switch_off.jpg';
+    }
+}
 
 
 knowledgeRunner()
